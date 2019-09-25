9a5b8783cc10c05f964ca1f6459a292907ff42de
mrpapercut <947626+mrpapercut@users.noreply.github.com>
2019-09-25 22:40:29 +0200

    Added binary-to-text encoder xxencoding

---
+++ src/Encoders/Xxencoding.js
@@ -0,0 +1,107 @@
+import Base from '../Base';
+
+class Xxenconding extends Base {
+    constructor() {
+        super();
+
+        this.chars = '+-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    }
+
+    encode(input) {
+        let inputArray = new Uint8Array(input.length);
+        for (let i in input) inputArray[i] = input.charCodeAt(i);
+
+        let charArr = new Array(3);
+
+        let len = input.length;
+        let [i, j, k, s] = [0, 0, 0, 0];
+
+        let lines = new Array();
+        let line = new Array();
+
+        while (len--) {
+            charArr[i++] = inputArray[k++];
+
+            if (i === 3) {
+                const remappedChars = this.remap3to4chars(charArr);
+
+                for (let n = 0; n < 4; n++) {
+                    line[s++] = this.chars[remappedChars[n]];
+                }
+
+                i = 0;
+            }
+
+            if (k % 45 === 0) {
+                lines.push(this.chars[45] + line.join(''));
+                line = new Array();
+            }
+        }
+
+        if (i) {
+            for (j = i; j < 3; j++) {
+                charArr[j] = 0;
+            }
+
+            const remappedChars = this.remap3to4chars(charArr);
+
+            for (let n = 0; n < 4; n++) {
+                line[s++] = remappedChars[n] === 0 ? '+' : this.chars[remappedChars[n]];
+            }
+        }
+
+        lines.push(this.chars[k > 45 ? k % 45 : k] + line.join(''));
+
+        return lines.join('\n') + '\n';
+    }
+
+    decode(input) {
+        const lines = input.split('\n');
+
+        const result = lines.reduce((acc, cur) => {
+            const curArr = this.stringToByteArray(cur);
+            const resArr = new Array(4);
+            const lineRes = [];
+
+            // Last line is of length 0
+            if (curArr.length === 0) return acc + '';
+
+            let origLen = this.chars.indexOf(String.fromCharCode(curArr[0]));
+            let curLen = curArr.length;
+            let i = 0;
+            let k = 1; // Skip first character because it indicates length
+
+            while (curLen--) {
+                resArr[i++] = curArr[k++];
+
+                if (i === 4) {
+                    lineRes.push(
+                        resArr.map(c => {
+                            let index = this.chars.indexOf(String.fromCharCode(c));
+                            if (index !== -1) {
+                                return index.toString(2).padStart(6, 0);
+                            }
+                        })
+                        .join('')
+                        .match(/(.{8})/g)
+                        .map(m => String.fromCharCode(parseInt(m, 2)))
+                        .join('')
+                    );
+
+                    i = 0;
+                }
+            }
+
+            let decodedLine = lineRes.join('');
+            if (decodedLine.length > origLen) {
+                decodedLine = decodedLine.substring(0, origLen);
+            }
+
+            return acc + decodedLine;
+        }, '');
+
+        return result;
+    }
+}
+
+export default Xxenconding;
---
+++ tests/Encoders/Xxencoding.spec.js
@@ -0,0 +1,38 @@
+import Xxencoding from '../../src/Encoders/Xxencoding';
+
+const encoder = new Xxencoding();
+
+const shortString = 'Hello, world!';
+const shortStringProof = 'BG4JgP4wg65RjQalY6E++\n';
+
+test('Should encode short string using xxencoding', () => {
+    expect(encoder.encode(shortString)).toEqual(shortStringProof);
+});
+
+test('Should decode xxencoded short string', () => {
+    const encodedString = encoder.encode(shortString);
+    expect(encoder.decode(encodedString)).toEqual(shortString);
+});
+
+const longString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel suscipit urna, ac auctor purus. Ut a nisl aliquam, pulvinar tortor quis, vehicula elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus sollicitudin tempor vestibulum. Pellentesque rutrum accumsan euismod. Suspendisse eros nunc, venenatis eu mi ut, convallis malesuada metus.';
+const longStringProof = `hH4xmNKoUOL-nRKoUN4xgPr6UQqZo643hNLEg64BjPbBZMrFZR5Jm643YOL-d
+hQqBdPaQUNKldR0sUEKldQLJVPG-qNKkUQrJnMqZkOLEURL7iMGkUMKAUMLJX
+hR4xm65-pQbJn9W-JR0-V64tdQqkUMKldQLJVPGkUQ5JgRaZiML6UR4xmR4xm
+h653pOLAg65NZO4ZXRKlV64JgOLEi63NZQrFdMbJgRKoUMKtoNG-dQ5BpPG-k
+hQaZhOLAUOKsUNa3pMqZWRLAUPr7XOG-gRKBoRLAUNLEURKloQaZXNLAUQ4xn
+hRKJmNG-XRK7dP4ZV62BpQa3ZCm-DQaBd65NVQaZpQm-iMLFjQLJZ65-ZPa3o
+hOK7pQm-ZR0-hMKRiOLAUN4Zn65-VQbFpQaZZPbEUPKxiR4Jn90-iMLBXNLFp
+hQW-mOKFdMrJgRLAUPLJn9W-EO43nNKlgRLAUQqxgP4ZXOLFpN4Zi65FZPL-j
+hQW-qNLBoOK7pP5Jh9W-ENKlgNKtoNLBlRKIUQbJoQbJh643XMrJhQq3i64Jp
+hOLBhPqEi63BpQr-ZPaFdQrBZ64JmPrAUPbJiMmkURaJiNKtVR4Zn64Jp64pd
+T65Jo90-XPqtqMKlgOLAUPK3gNLBpMKFV64pZR5Jn9U++
+`;
+
+test('Should encode long string using xxencoding', () => {
+    expect(encoder.encode(longString)).toEqual(longStringProof);
+});
+
+test('Should decode xxencoded long string', () => {
+    const encodedString = encoder.encode(longString);
+    expect(encoder.decode(encodedString)).toEqual(longString);
+});
