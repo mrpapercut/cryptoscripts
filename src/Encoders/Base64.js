af11c6b79e0ab43c44be9b351ea0c8876f54e1d1
mrpapercut <947626+mrpapercut@users.noreply.github.com>
2019-09-25 22:39:06 +0200

    Cleaned up code

--- src/Encoders/Base64.js
+++ src/Encoders/Base64.js
@@ -8,54 +8,45 @@ class Base64 extends Base {
     }
 
     encode(input) {
-        let inputArray = [];
-
-        for (let i in input) {
-            inputArray.push(input[i].charCodeAt(0));
-        }
-
+        let inputArray = this.stringToByteArray(input);
         let length = inputArray.length;
 
-        let [i, j, k, s] = [0, 0, 0, 0];
+        const charArr = new Array(3);
+        const b64Arr = new Array();
 
-        let char_array_3 = new Array(3);
-        let char_array_4 = new Array(4);
-
-        const b64len = (length + 2 - ((length + 2) % 3)) * 4 / 3;
-        const b64str = new Array(b64len + 1);
-
+        let i = 0; // Count 0-3 in charArr
+        let j = 0; // Counts through all characters in inputArray
+        let s = 0; // Counts through all characters in output
         while (length--) {
-            char_array_3[i++] = inputArray[k++];
+            charArr[i++] = inputArray[j++];
             if (i == 3) {
-                char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-                char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-                char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-                char_array_4[3] = char_array_3[2] & 0x3f;
+                const remappedChars = this.remap3to4chars(charArr);
 
-                for (i = 0; i < 4; i++)
-                    b64str[s++] = this.base64_chars[char_array_4[i]];
+                for (let k = 0; k < 4; k++) {
+                    b64Arr[s++] = this.base64_chars[remappedChars[k]];
+                }
 
                 i = 0;
             }
         }
 
         if (i) {
-            for (j = i; j < 3; j++)
-                char_array_3[j] = '\0';
+            for (let l = i; l < 3; l++) {
+                charArr[l] = '\0';
+            }
 
-            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-            char_array_4[3] = char_array_3[2] & 0x3f;
+            const remappedChars = this.remap3to4chars(charArr);
 
-            for (j = 0; j < i + 1; j++)
-                b64str[s++] = this.base64_chars[char_array_4[j]];
+            for (let m = 0; m < i + 1; m++) {
+                b64Arr[s++] = this.base64_chars[remappedChars[m]];
+            }
 
-            while (i++ < 3)
-                b64str[s++] = '=';
+            while (i++ < 3) {
+                b64Arr[s++] = '=';
+            }
         }
 
-        return b64str.join('');
+        return b64Arr.join('');
     }
 
     decode(input) {
