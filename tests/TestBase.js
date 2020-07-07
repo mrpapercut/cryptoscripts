02ca980e66a24992f890641bee7b769c6679f6ae
mrpapercut <947626+mrpapercut@users.noreply.github.com>
2020-07-07 19:47:43 +0200

    Adding function loadSourcefile

--- tests/TestBase.js
+++ tests/TestBase.js
@@ -1,2 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+
 export const SHORTSTRING = 'Hello, world!';
+
 export const LONGSTRING = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel suscipit urna, ac auctor purus. Ut a nisl aliquam, pulvinar tortor quis, vehicula elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus sollicitudin tempor vestibulum. Pellentesque rutrum accumsan euismod. Suspendisse eros nunc, venenatis eu mi ut, convallis malesuada metus.';
+
+export const loadSourcefile = relpath => {
+    return new Promise((resolve, reject) => {
+
+        fs.readFile(path.resolve(__dirname, relpath), (err, contents) => {
+            if (err) {
+                if (err.code === 'ENOENT') {
+                    reject(`File ${relpath} does not exist`);
+                }
+
+                throw err;
+            }
+
+            resolve(contents);
+        })
+    });
+}
